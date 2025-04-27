a056e1f1737e08986af215d74ad376bb46f6b997
XdTechDev <arohiraju238@gmail.com>
2025-04-27 21:21:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Zaynix-MD=OJwiGT7Y#yMKDUpED5iiLVGvq9isq86REUbxcZ7--dbELOFlrjkg", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "Zaynix-MD=LJYW3KBC#DTk-F6qWU6-Cb04DRE9dMc8NUvmHcHcp2TzbIkSDJMo", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://sulabijja:sulabijja@demon.d4ov0.mongodb.net/?retryWrites=true&w=majority&appName=DEMON", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/liepk0.jpg",
 BOT_NAME: process.env.BOT_NAME || "Zaynix-MD",
