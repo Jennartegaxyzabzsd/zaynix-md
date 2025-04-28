62f8a6216c10b08c2771c8d3c0c009f1125dbaac
XdTechDev <arohiraju238@gmail.com>
2025-04-28 15:10:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Zaynix-MD=LJYW3KBC#DTk-F6qWU6-Cb04DRE9dMc8NUvmHcHcp2TzbIkSDJMo", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://sulabijja:sulabijja@demon.d4ov0.mongodb.net/?retryWrites=true&w=majority&appName=DEMON", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/liepk0.jpg",
-BOT_NAME: process.env.BOT_NAME || "Zaynix-MD",
+BOT_NAME: process.env.BOT_NAME || "Zaynix-MDt",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
