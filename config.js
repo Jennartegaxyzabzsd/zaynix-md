de8a02a91039d6aa64a5ecf3de83ad031ff7a793
Jennartegaxyzabzsd <arohiraju238@gmail.com>
2025-06-01 09:09:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   // --- Requiretd Core Configurations ---
-  SESSION_ID: process.env.SESSION_ID || "Zaynix-MD=KQInGITA#7wfzpZX9wAFep_i2c8ir_9HLfSyxwYLitGT1hZB_jJ0", // Your session ID
+  SESSION_ID: process.env.SESSION_ID || "Zaynix-MD=qNYg1L5Y#dyOtmPDAL96J2VuyKvxCRf1G9IXOqdU7xW2pjuZFQe0", // Your session ID
   MONGODB: process.env.MONGODB || "mongodb+srv://oyysreejan8:r9f3q8OpBE9UdFEv@cluster0.h3mzhuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // Your MongoDB URL
 
   // --- Optional APIs ---
